fdb68baf29c1db5545a8b10577a0dc2fb67d8af8
Dech <61090003@kmitl.ac.th>
2021-12-04 04:40:51 +0700

    setup library and create splash screen 2

---
+++ app/src/main/java/com/example/dechproduct/hotelreservationapp/presentation/hotel/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.dechproduct.hotelreservationapp.presentation.hotel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private val runnable = Runnable {
+        if (!isFinishing){
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable,2500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
